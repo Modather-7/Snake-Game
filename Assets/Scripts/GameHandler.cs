634d82b35f89b9f3c104714aa76202b0adf6cd15
Modather-7 <modatherelbeltagy@std.mans.edu.eg>
2026-04-24 17:55:53 +0300

    Added snake game project files

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    [SerializeField] private Snake snake;
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+
+        GameObject snakeHeadGameObject  = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+    }
+
+}
